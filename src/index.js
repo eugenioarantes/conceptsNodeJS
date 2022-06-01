351619e66c2e4e7a1a957d1b7dc87ebafccd4f66
Eugenio <eugeniolopesarantesjunior@gmail.com>
2022-06-01 14:26:37 -0300

    create http methods

--- src/index.js
+++ src/index.js
@@ -2,8 +2,45 @@ const express = require('express');
 
 const app = express();
 
-app.get( '/', (request, response) => {
-  return response.json({ message:'Hello world' });
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar informações do back-end
+ * POST: Criar uma informação no back-end
+ * PUT/PATCH: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+app.get( '/projects', (request, response) => {
+  return response.json([
+    'Project 1',
+    'Project 2',
+  ]);
 });
 
-app.listen(3333);
+app.post( '/projects', (request, response) => {
+  return response.json([
+    'Project 1',
+    'Project 2',
+    'Project 3',
+  ]);
+});
+
+app.put( '/projects/:id', (request, response) => {
+  return response.json([
+    'Project 4',
+    'Project 5',
+    'Project 6',
+  ]);
+});
+
+app.delete( '/projects/:id', (request, response) => {
+  return response.json([
+    'Project 6',
+  ]);
+});
+
+
+app.listen(3333, () => {
+  console.log('🚀Back-end started!');
+});
